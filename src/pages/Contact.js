132cfc481c29d7a9830c1394711b47528a71bf3c
Abhi <abhigangwarlife@gmail.com>
2024-11-30 10:43:45 +0530

    color

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -212,7 +212,7 @@ const ContactUs = () => {
               onChange={handleChange}
               className="w-full p-3 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               rows="5"
-              required
+             
             />
           </div>
 
